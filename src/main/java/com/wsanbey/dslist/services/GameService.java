6a2d95e1033d0d972267642a4cfc96713e7973ed
Wsanbey <welrysanbey@hotmail.com>
2024-05-08 13:16:15 -0300

    GET game by id

--- src/main/java/com/wsanbey/dslist/services/GameService.java
+++ src/main/java/com/wsanbey/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.wsanbey.dslist.services;
 
+import com.wsanbey.dslist.dto.GameDTO;
 import com.wsanbey.dslist.dto.GameMinDTO;
 import com.wsanbey.dslist.entities.Game;
 import com.wsanbey.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
